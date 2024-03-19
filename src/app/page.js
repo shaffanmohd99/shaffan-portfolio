d3c3aa3468770509eef7c4b10fb9e980b8f3dee0
shaffanmohd <shaffanmohd@gmail.com>
2024-03-19 09:27:06 +0800

    fix text

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,12 @@ import useElementOnScreen from "@/hooks/useElementOnScreen";
 import { NavigationContext } from "@/context/navigationContext";
 import { useContext, useEffect } from "react";
 
+const text = [
+  "I graduated from the University of Malaya with a bachelor's degree in mechanical engineering back in 2022. My first exposure to tech was during my internship days as a product manager. Little did I know, this experience would open my eyes to the world of product development, sparking my interest in software engineering.",
+  "When I was applying for my first job, I stumbled upon an exciting opportunity to delve into software development. It was a chance for me to embark on a journey toward becoming a full-stack software developer while gaining practical experience in a professional setting. Without hesitation, I seized the opportunity, eager to immerse myself in the dynamic world of software engineering.",
+  "Now I realised that the tech world is always changing, and I know I need to keep up. That's why I'm constantly seeking out new ways to learn and grow. Whether it's through online courses or hands-on projects, I'm always eager to expand my skillset and stay ahead of the curve.",
+];
+
 export default function Home() {
   const [refCallback, isVisible] = useElementOnScreen();
   const { setposition } = useContext(NavigationContext);
@@ -20,28 +26,11 @@ export default function Home() {
           About
         </Typography>
         <div className="flex flex-col gap-3">
-          <Typography variant="p">
-            I graduated from the University of Malaya with a bachelor's degree in
-            mechanical engineering back in 2022. My first exposure to tech was
-            during my internship days as a product manager. Little did I know,
-            this experience would open my eyes to the world of product
-            development, sparking my interest in software engineering.
-          </Typography>
-          <Typography variant="p">
-            When I was applying for my first job, I stumbled upon an exciting
-            opportunity to delve into software development. It was a chance for me
-            to embark on a journey toward becoming a full-stack software developer
-            while gaining practical experience in a professional setting. Without
-            hesitation, I seized the opportunity, eager to immerse myself in the
-            dynamic world of software engineering.
-          </Typography>
-          <Typography variant="p">
-            Now I realised that the tech world is always changing, and I know I
-            need to keep up. That's why I'm constantly seeking out new ways to
-            learn and grow. Whether it's through online courses or hands-on
-            projects, I'm always eager to expand my skillset and stay ahead of the
-            curve.
-          </Typography>
+          {text.map((item, index) => (
+            <Typography key={index} variant="p">
+              {item}
+            </Typography>
+          ))}
         </div>
       </div>
       <div id="experience" ref={refCallback} className="   py-8 mb-24">
